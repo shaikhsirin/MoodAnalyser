55914b573c2ebe363b4c87a6e5c333c25747cc74
shaikhsirin <shaikhsirin8080example.com>
2022-04-04 11:05:40 +0530

    Added:- Analysing Mood of Sad

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,27 @@
+/**
+ * purpose : analyse and respond Happy or Sad Mood
+ *@author : Sirin
+ * since : 04-04-2022
+ */
+
+public class MoodAnalyser {
+    public static void main(String args[]) {
+        System.out.println("Mood Analyser");
+
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String message = moodAnalyser.analyseMood("I am Happy");
+        System.out.println(message);
+        message = moodAnalyser.analyseMood("I am Sad");
+        System.out.println(message);
+    }
+    public String analyseMood(String message) {
+        if (message.toLowerCase().contains("happy")) {
+            return "HAPPY";
+        } else if (message.toLowerCase().contains("sad")) {
+            return "SAD";
+        } else {
+            return null;
+        }
+    }
+}
+
