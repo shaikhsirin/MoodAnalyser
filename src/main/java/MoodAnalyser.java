0bd0f073e676b55b2e38393e37b04b847371f2e1
shaikhsirin <shaikhsirin8080example.com>
2022-04-04 11:14:50 +0530

    Added:- Analysing Any Mood Happy Or Sad

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -15,13 +15,15 @@ public class MoodAnalyser {
         System.out.println(message);
     }
     public String analyseMood(String message) {
-        if (message.toLowerCase().contains("happy")) {
+        if (message.toLowerCase().contains("happy")) || message.toLowerCase().contains("any")) {
             return "HAPPY";
-        } else if (message.toLowerCase().contains("sad")) {
+        }
+        else if (message.toLowerCase().contains("sad")) {
             return "SAD";
         } else {
             return null;
         }
     }
+
 }
 
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -9,6 +9,12 @@ public class MoodAnalyserTest {
         String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
         Assert.assertEquals("SAD", actualResult);
     }
+    @Test
+    public void analyseAnyMood(Assertions Assert) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String actualResult = moodAnalyser.analyseMood("I am Any in Mood");
+        Assert.assertEquals("HAPPY", actualResult);
+    }
 
 }
 
